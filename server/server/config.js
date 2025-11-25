49ac85d4cc4d82fd46a0928db4283fc10b585964
Utkarsh <fanaticvrun09@gmail.com>
2025-11-25 18:09:28 +0530

    Add fallback JWT secret for deployment

--- server/server/config.js
+++ server/server/config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 4000,
   mongoUri: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
+  jwtSecret: process.env.JWT_SECRET || 'quizify-secret-key-2024',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d"
 };
